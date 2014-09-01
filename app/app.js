ecf7de97a0b96ae47bbae0f98e4e773c4304e744
Alan Rubin <alan@frubin.com>
2014-09-01 20:05:44 +0300

    Moving ui-router state config to inside module

--- app/app.js
+++ app/app.js
@@ -1,14 +1,7 @@
 'use strict';
 
 angular.module('openTrain', ['otDelay', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('delay', {
-        url: "/delay",
-        templateUrl: 'modules/delay/template.html',
-        controller: 'DelayController'
-      });
-
-    $urlRouterProvider.otherwise("/delay");
-  })
-;
+  .config(function ($urlRouterProvider) {
+  	// Default is /delay
+    $urlRouterProvider.otherwise('/delay');
+  });
--- app/modules/delay/module.js
+++ app/modules/delay/module.js
@@ -1,3 +1,10 @@
 'use strict';
 
-angular.module('otDelay', []);
+angular.module('otDelay', ['ui.router']).config(function($stateProvider) {
+	$stateProvider
+		.state('delay', {
+			url: '/delay',
+			templateUrl: 'modules/delay/template.html',
+			controller: 'DelayController'
+		});
+});
