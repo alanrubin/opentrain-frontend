6f7d64a9d62fab031430a5043609007d0065c310
Alan Rubin <alan@frubin.com>
2014-09-22 20:39:52 +0300

    UI-View for side info panel

--- app/app.js
+++ app/app.js
@@ -3,5 +3,5 @@
 angular.module('openTrain', ['otDashboard', 'otAbout', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
   .config(function ($urlRouterProvider) {
   	// Default is /
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/dashboard');
   });
--- app/modules/dashboard/module.js
+++ app/modules/dashboard/module.js
@@ -3,8 +3,12 @@
 angular.module('otDashboard', ['ui.router']).config(function($stateProvider) {
 	$stateProvider
 		.state('dashboard', {
-			url: '/',
+			url: '/dashboard',
 			templateUrl: 'modules/dashboard/template.html',
 			controller: 'DashboardController'
+		})
+		.state('dashboard.schedule', {
+			url: '/schedule',
+			templateUrl: 'modules/dashboard/schedule.panel.template.html'
 		});
 });
