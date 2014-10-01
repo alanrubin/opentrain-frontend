75b09d59f8867bd2fa438ef338af6d7114966e7d
Alan Rubin <alan@frubin.com>
2014-10-01 15:38:57 +0300

    Adding ng translate for multiple languages

--- app/app.js
+++ app/app.js
@@ -1,7 +1,27 @@
 'use strict';
 
-angular.module('openTrain', ['otDashboard', 'otAbout', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap'])
-  .config(function ($urlRouterProvider) {
-  	// Default is /
-    $urlRouterProvider.otherwise('/dashboard/area');
-  });
+angular.module('openTrain', ['otDashboard', 'otAbout', 'ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'pascalprecht.translate'])
+	.config(function($urlRouterProvider, $translateProvider) {
+		// Default is /
+		$urlRouterProvider.otherwise('/dashboard/area');
+
+		// Translation configuration
+		$translateProvider
+			.translations('en', {
+				HEADER: {
+					TITLE: 'OpenTrain',
+					ABOUT_US: 'About us',
+					HASADNA: 'Hasadna',
+					NAVIGATION: 'Toggle navigation'
+				}
+			})
+			.translations('he', {
+				HEADER: {
+					TITLE: 'רכבת פתוחה',
+					ABOUT_US: 'עלינו',
+					HASADNA: 'הסדנא',
+					NAVIGATION: 'לאפשר ניווט'
+				}
+			})
+			.preferredLanguage('he');
+	});
